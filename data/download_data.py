bdf9b9ddce7dfbd79bb2a8ef62c400b07f834ffc
Bob Chen <bob_chen@fastmail.com>
2022-08-19 19:10:04 -0700

    WIP: Load datasets into Postgres2

--- data/download_data.py
+++ data/download_data.py
@@ -1,8 +1,8 @@
-import openml
-import numpy as np
-import pickle
-from sklearn.preprocessing import LabelEncoder
 import os
+import openml
+import psycopg2
+import pandas
+
 openml.config.cache_directory = os.path.expanduser(os.getcwd() + "/openml_cache")
 
 def save_suite(suite_id, dir_name, save_categorical_indicator=False, regression=True):
@@ -27,6 +27,39 @@ def save_suite(suite_id, dir_name, save_categorical_indicator=False, regression=
         # At this point, `X` and `y` are `pandas.DataFrame`s.
         # `categorical_indicator` is a `List[bool]`.
         # TODO: Store `X` and `y` into a Postgres database.
+
+def load_into_postgres(df, table_name):
+    conn = psycopg2.connect(
+        database="tabular_benchmark",
+        user="postgres", 
+        password="postgres", 
+        host="localhost", 
+        port= "5432",
+    )
+    
+    conn.autocommit = True
+    cursor = conn.cursor()
+    
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    
+    sql = """CREATE TABLE airlines_final(id int ,
+    day int ,airline char(20),destination char(20));"""
+    
+    cursor.execute(sql)
+    
+    data: pandas.DataFrame = df
+
+    # converting data to sql
+    data.to_sql(table_name, conn, if_exists= "replace")
+    
+    # fetching all rows
+    sql1="""select * from airlines_final;"""
+    cursor.execute(sql1)
+    for i in cursor.fetchall():
+        print(i)
+    
+    conn.commit()
+    conn.close()
                 
 suites_id = {"numerical_regression": 297,
           "numerical_classification": 298,
